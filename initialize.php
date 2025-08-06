fe7fb8bf9832e68ba48a3516882d1e73559b1e7f
Sunday Dominic <sunnyemmanuel00@gmail.com>
2025-08-06 02:37:54 +0100

    Fix: Set dynamic base_url for Render deployment

--- initialize.php
+++ initialize.php
@@ -27,4 +27,4 @@ if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',""); 
 if(!defined('DB_NAME')) define('DB_NAME',"banking_db");
 
- ?>
+?>
